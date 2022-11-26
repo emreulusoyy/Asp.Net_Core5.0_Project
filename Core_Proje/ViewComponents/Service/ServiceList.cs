85517da76347db654c1994cde86da76c38f03ee0
ulusoydev <eulusoy139@gmail.com>
2022-11-26 03:11:55 +0300

    26.11

---
+++ Core_Proje/ViewComponents/Service/ServiceList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Service
+{
+    public class ServiceList : ViewComponent
+    {
+        ServiceManager servicemanager = new ServiceManager(new EfServiceDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = servicemanager.TGetList();
+            return View(values);
+        }
+    }
+}
