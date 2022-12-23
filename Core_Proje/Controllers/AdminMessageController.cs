08a903205bdd1aa7195b5622477133291a56e608
ulusoydev <eulusoy139@gmail.com>
2022-12-23 20:41:48 +0300

    a

--- Core_Proje/Controllers/AdminMessageController.cs
+++ Core_Proje/Controllers/AdminMessageController.cs
@@ -1,6 +1,10 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 
 namespace Core_Proje.Controllers
 {
@@ -21,6 +25,35 @@ namespace Core_Proje.Controllers
             p = "admin@gmail.com";
             var values = writermessagemanager.GetListSendMessage(p);
             return View(values);
+        }
+
+        public IActionResult AdminMessageDetails(int id)
+        {
+            var values = writermessagemanager.TGetByID(id);
+            return View(values);
+        }
+        public IActionResult AdminMessageDelete(int id)
+        {
+            var values = writermessagemanager.TGetByID(id);
+            writermessagemanager.TDelete(values);
+            return RedirectToAction("SenderMessageList");
+        }
+        [HttpGet]
+        public IActionResult AdminMessageSend()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AdminMessageSend(WriterMessage p)
+        {
+            p.Sender = "admin@gmail.com";
+            p.SenderName = "Admin";
+            p.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            Context c = new Context();
+            var usernamesurname = c.Users.Where(x => x.Mail == p.Reciver).Select(y => y.Name + " " + y.Surname).FirstOrDefault();
+            p.ReciverName = usernamesurname;
+            writermessagemanager.TAdd(p);
+            return RedirectToAction("SenderMessageList");
         }
     }
 }
